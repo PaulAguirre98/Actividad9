ffd9c1cf22ae1f0c27ff27638049c10085e6b105
Alfredo Paul Aguirre Torres <alfpaul98@hotmail.com>
2018-11-06 22:38:01 -0600

    Initial commit

---
+++ app/src/main/java/mipp/com/actividad9_2/BroadcastReceptor.java
@@ -0,0 +1,14 @@
+package mipp.com.actividad9_2;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.widget.Toast;
+
+public class BroadcastReceptor extends BroadcastReceiver {
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        Toast.makeText(context, "Transmisión Recibida", Toast.LENGTH_SHORT).show();
+    }
+}
